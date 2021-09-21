2e0d8d95db872198701a8b0c970a735085bb8b6a
devocrw <devocrw@gmail.com>
2021-09-21 16:00:50 +0200

    Ajout fonction logs
    Journal d'info et erreur

---
+++ Backend/config/logger.js
@@ -0,0 +1,25 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Écrire tous les journaux de type `erreur` dans `error.log`.
+    // - Ecriture de tous les journaux de type `info` dans le fichier `combined.log`.
+
+    new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: './logs/combined.log' }),
+  ],
+});
+
+
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
