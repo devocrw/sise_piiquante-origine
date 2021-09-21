1493d3cf32339ab4e31fbb10c82ec542ee378066
devocrw <devocrw@gmail.com>
2021-09-21 15:32:46 +0200

    Modification models
    sauce

--- Backend/routes/sauce.js
+++ Backend/routes/sauce.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+const sauceCtrl = require('../controllers/sauce');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 //////////////////// ROUTES SAUCES - ENREGISTREMENT DANS LA BASE DE DONNÉES ////////////////////
 
 // Envoyer les sauces dans la base de données
-router.post('/', sauceCtrl.createSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
 
 // Mise à jour de la sauce grace à l'id fourni
-router.put('/:id', sauceCtrl.modifySauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 
 // Supprimer la sauce de la base de données
-router.delete('/:id', sauceCtrl.deleteSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
 // Tableaux des sauces
-router.get('/', sauceCtrl.getAllSauces);
+router.get('/', auth, sauceCtrl.getAllSauces);
 
 // Sauce unique - renvoie la sauce avec l'Id fournit
-router.get('/:id', sauceCtrl.getOneSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+
+// Liker/disliker les sauces
+router.post('/:id/like', auth, sauceCtrl.likeDislikeSauce);
 
 module.exports = router;
